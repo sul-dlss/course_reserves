07d30a98782778bd1fbfd7ed5bbcce14160e45dc
Jonathan Lavigne <jlavigne@stanford.edu>
2012-01-31 17:10:20 -0800

    Initial commit for this application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseReserves::Application.config.secret_token = '075336dbaed61418fa44621043330273511b7345aa936cb65f52bfbf514abbe8a64c24e0502fa35800a57f1b3154d10f764cb097b74650d963690e8faafc616b'
