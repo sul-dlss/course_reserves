7e01d02c95bbeeded06970b501e388b9cd00aca1
Jonathan Lavigne <jlavigne@stanford.edu>
2012-02-22 14:36:34 -0800

    Fixed test to use create

--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -4,14 +4,13 @@ describe Reserve do
 
   describe "item_list serialization" do 
     it "should serialize the item list" do 
-      reserve = Reserve.new( { :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] } )
+      reserve = Reserve.create( { :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] } )
       reserve.save!
       reserve[:item_list].should == [{ :title => 'My Title' }]
     end 
     
     it "should throw an error for TypeMismatch when we serialize the item list with a hash" do 
-      reserve = Reserve.new( { :cid => 'test_cid', :item_list => { :title => 'My Title' } } )
-      lambda {reserve.save!}.should raise_error(ActiveRecord::SerializationTypeMismatch)
+      lambda {Reserve.create( { :cid => 'test_cid', :item_list => { :title => 'My Title' } } )}.should raise_error(ActiveRecord::SerializationTypeMismatch)
     end 
 
   end
