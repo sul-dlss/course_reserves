7a4a328f2b04438675b9bc696dae4be38ee6e8fd
Jonathan Lavigne <jlavigne@stanford.edu>
2012-02-23 15:04:04 -0800

    Commented out line in controller

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,27 +1,21 @@
 class ReservesController < ApplicationController
   
   def index
-    @my_reserves = Editor.find_by_sunetid(request.env['WEBAUTH_ID']).reserves
+    #@my_reserves = Editor.find_by_sunetid(request.env['WEBAUTH_USER']).reserves
   end
   
-  def create 
-    
+  def create     
     @reserve = Reserve.create(params[:reserve])
     @reserve.save!
-    
   end
   
-  def edit 
-    
-    @reserve = Reserve.find(params[:id])
-    
+  def edit    
+    @reserve = Reserve.find(params[:id])    
   end
   
-  def update
-    
+  def update    
     Reserve.update(params[:reserve][:id], params[:reserve])
     redirect_to edit_reserve(params[:reserve][:id])
-    
   end
   
   def show
