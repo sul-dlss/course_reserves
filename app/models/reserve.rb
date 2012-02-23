b17991c983375f4d098e3bd12b29dec45138333d
Jonathan Lavigne <jlavigne@stanford.edu>
2012-02-22 17:07:36 -0800

    Changes to test reserves and editors relationships

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,14 +1,25 @@
 class Reserve < ActiveRecord::Base
+  
+  before_create :process_sunet_ids 
 
   has_and_belongs_to_many :editors
   
   serialize :item_list, Array
   
-  #def self.editors
- 
+  private
+  
+  def process_sunet_ids
     
- 
-  #end
+    editors = []
+    unless self.editor_sunet_ids.blank?
+      self.editor_sunet_ids.split(/,/).map{|i| i.strip}.each do |s|
+        editors << Editor.find_or_create_by_sunetid(s) 
+      end
+    end
+
+    self.editors = editors unless editors.blank?
+    
+  end
   
 
 end
--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -2,13 +2,22 @@ require 'spec_helper'
 
 describe Reserve do
   
-  describe "editor relationships" do
+  describe "editor relationships" do    
     
-    it "should add an editor relationship" do
-      reserve = Reserve.create( { :editors => [Editor.create({:sunetid => 'test'})], :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] } )
+    it "should generate editor relationships from editor_sunet_ids field for single sunet_id" do
+      reserve = Reserve.create( { :editor_sunet_ids => 'jlavigne', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] } )
       reserve.save!
       reserve.editors.length.should==1
-      reserve.editors.first[:sunetid].should=='test'     
+      reserve.editors.first[:sunetid].should=='jlavigne'   
+    end
+    
+    it "should generate editor relationships from editor_sunet_ids field for multiple sunet_id" do
+      reserve = Reserve.create( { :editor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] } )
+      reserve.save!
+      reserve.editors.length.should==2
+      editors = reserve.editors.map{|e| e[:sunetid] }
+      editors.include?('jlavigne').should==true
+      editors.include?('jkeck').should==true
     end
     
   end
