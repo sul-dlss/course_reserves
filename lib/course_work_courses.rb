763bcd297a72af48b942ab42612c8c4a506f1e4c
Jessie Keck <jessie.keck@gmail.com>
2012-02-24 13:38:08 -0800

    More finders, more tests, and an additional way to initialize object w/ arbitrary XML.

--- lib/course_work_courses.rb
+++ lib/course_work_courses.rb
@@ -1,11 +1,15 @@
 require 'nokogiri'
 class CourseWorkCourses
-  def initialize(xml)
-    # Need to load XML.  For now, we'll load from the fixtures directory.
+  def initialize(xml=nil)
+    if xml
+      @raw_xml = Nokogiri::XML(xml)
+    else
+      @raw_xml = load_xml_from_coursework
+    end
   end
   
   def raw_xml
-    @raw_xml ||= Nokogiri::XML(File.open("#{Rails.root}/spec/fixtures/course_work.xml", 'r'))
+    @raw_xml ||= self.raw_xml
   end
   
   def find_by_sunet(sunet)
@@ -14,17 +18,39 @@ class CourseWorkCourses
     end.compact
   end
   
+  def find_by_class_id(class_id)
+    self.all_courses.map do |course|
+      course if course[:cid] == class_id
+    end.compact
+  end
+  
+  def find_by_class_id_and_section(class_id, section)
+    self.all_courses.map do |course|
+      course if course[:cid] == class_id and course[:sid] == section
+    end.compact
+  end
+  
+  def find_by_class_id_and_section_and_sunet(class_id, section, sunet)
+    self.all_courses.map do |course|
+      course if course[:cid] == class_id and course[:sid] == section and course[:instructors].map{|i| i[:sunet]}.include?(sunet)
+    end.compact
+  end
+  
   def all_courses
     @all_course ||= process_all_courses_xml(self.raw_xml)
   end
   
-  
   private
+  
+  def load_xml_from_coursework
+    Nokogiri::XML(File.open("#{Rails.root}/spec/fixtures/course_work.xml", 'r'))
+  end
   
   def process_all_courses_xml(xml)
     courses = []
     xml.xpath("//courseclass").each do |course|
-      course_title = course[:title]      
+      course_title = course[:title]
+      term = course[:term]
       course.xpath("./class").each do |cl|
         class_id = cl[:id]
         cl.xpath("./section").each do |sec|
@@ -33,11 +59,11 @@ class CourseWorkCourses
           sec.xpath("./instructors/instructor").each do |inst|
             instructors << {:sunet=>inst[:sunetid], :name => inst.text}
           end
-          courses << { :title=>course_title, 
-                       :cid => class_id,
-                       :sid => section_id,
-                       :instructors => instructors
-                     }
+          courses << {:title       => course_title,
+                      :term        => term,
+                      :cid         => class_id,
+                      :sid         => section_id,
+                      :instructors => instructors}
         end        
       end
     end
--- spec/lib/course_work_courses_spec.rb
+++ spec/lib/course_work_courses_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 require "course_work_courses"
 describe "CourseWorkCourses" do
   before(:all) do
-    @courses = CourseWorkCourses.new("xml")
+    @courses = CourseWorkCourses.new
   end
-  describe "raw xml from course works" do
+
+  describe "loading raw XML" do
     it "should be a Nokogiri XML Document" do
       @courses.raw_xml.is_a?(Nokogiri::XML::Document).should be_true
+    end
+    it "should load XML when provided on object initialization" do
+      courses = CourseWorkCourses.new('<rsponse><courseclass title="MyTitle"><class id="CLASS-ID"><section id="01"><instructors><instructor sunetid="mysunet">My Teacher</instructor></instructors></section></class></courseclass></response>').all_courses
+      courses.length.should == 1
+      courses.first[:cid].should == "CLASS-ID"
+      courses.first[:sid].should == "01"
     end
   end
   
@@ -24,17 +31,70 @@ describe "CourseWorkCourses" do
     end
   end
   
-  describe "getting a course by SUNet ID" do
-    it "should return the subset of courses for the SUNet ID w/ fewer courses" do
-      courses_456 = @courses.find_by_sunet("456")
-      courses_456.length.should == 2
-      courses_456.map{|c| c[:title]}.should == ["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"]
+  describe "finders" do
+    describe "by SUNet ID" do
+      it "should return the subset of courses for the SUNet ID w/ fewer courses" do
+        courses_456 = @courses.find_by_sunet("456")
+        courses_456.length.should == 2
+        courses_456.map{|c| c[:title]}.should == ["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"]
+      end
+      it "should return the subset of courses for the SUNet ID that is an instructor for all courses" do
+        courses_123 = @courses.find_by_sunet("123")
+        courses_123.length.should == 5
+        courses_123.map{|c| c[:title]}.uniq.should == ["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"]
+      end
+      it "should be blank when a course doesn't exist with the specified SUNet" do
+        @courses.find_by_sunet("no-sunet").should be_blank
+      end
     end
-    it "should return the subset of courses for the SUNet ID that is an instructor for all courses" do
-      courses_123 = @courses.find_by_sunet("123")
-      courses_123.length.should == 5
-      courses_123.map{|c| c[:title]}.uniq.should == ["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"]
+  
+    describe "by class id" do
+      it "should return the appropriate course when passing a class id" do
+        two_section_course = @courses.find_by_class_id("W12-EDUC-237X")
+        two_section_course.length.should == 2
+        two_section_course.map{|c| "#{c[:cid]}-#{c[:sid]}"}.should == ["W12-EDUC-237X-01", "W12-EDUC-237X-02"]
+      end
+      it "should be blank when a course doesn't exist with the specified class id" do
+        @courses.find_by_class_id("DOES-NOT-EXIST").should be_blank
+      end
     end
+  
+    describe "by class id and section" do
+      it "should return the appropriate course when passing a class id and section id" do
+        course = @courses.find_by_class_id_and_section("W12-EDUC-237X", "01")
+        course.length.should == 1
+        course.first[:title].should == "Residential Racial Segregation and the Education of African-American Youth"
+        course.first[:cid].should == "W12-EDUC-237X"
+        course.first[:sid].should == "01"
+      end
+      it "should be blank when the class id doesn't exist" do
+        @courses.find_by_class_id_and_section("DOES-NOT-EXIST", "01").should be_blank
+      end
+      it "should be blank when the class id is valid but not the section" do
+        @courses.find_by_class_id_and_section("W12-EDUC-237X", "03").should be_blank
+      end
+    end
+    
+    describe "by class id, section, and SUNet ID" do
+      it "should return the appripriate course when passing all valid information" do
+        course = @courses.find_by_class_id_and_section_and_sunet("W12-EDUC-237X", "01", "123")
+        course.length.should == 1
+        course.first[:title].should == "Residential Racial Segregation and the Education of African-American Youth"
+        course.first[:cid].should == "W12-EDUC-237X"
+        course.first[:sid].should == "01"
+        course.first[:instructors].map{|i| i[:sunet] }.include?("123").should be_true
+      end
+      it "should return blank when the class id does not exist" do
+        @courses.find_by_class_id_and_section_and_sunet("DOES-NOT-EXIST", "01", "123").should be_blank
+      end
+      it "should return blank when the section does not exist for the given class id" do
+        @courses.find_by_class_id_and_section_and_sunet("W12-EDUC-237X", "03", "123").should be_blank
+      end
+      it "should return blank when the section SUNet isn't an instructor in for the given course id and section" do
+        @courses.find_by_class_id_and_section_and_sunet("W12-EDUC-237X", "02", "456").should be_blank
+      end
+    end
+    
   end
   
 end
