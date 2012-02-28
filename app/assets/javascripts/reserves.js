9b3109c7cf7c781fb244dcb34b22e1a858fd0392
Jessie Keck <jessie.keck@gmail.com>
2012-02-28 14:37:26 -0800

    Adding jQuery for jQuery UI dialogs.

--- app/assets/javascripts/reserves.js
+++ app/assets/javascripts/reserves.js
@@ -19,6 +19,39 @@ $(document).ready(function(){
 		update_item_list_numbers_and_classes();
 		return false;
 	});
+
+  // jQuery dialog
+  $("a.dialog").each(function() {
+    var dialog_box = "empty";
+    var link = $(this);
+    $(this).click( function() {     
+      //lazy create of dialog
+      if ( dialog_box == "empty") {
+        dialog_box = $('<div class="dialog_box"></div>').dialog({ autoOpen: false});          
+      }
+      // Load the original URL on the link into the dialog associated
+      // with it. Rails app will give us an appropriate partial.
+      // pull dialog title out of first heading in contents. 
+      $("body").css("cursor", "progress");
+      dialog_box.load( this.href , function() {
+			  // Remove first header from loaded content, and make it a dialog
+	      // title instead
+	      var heading = dialog_box.find("h1, h2, h3, h4, h5, h6").eq(0).remove();
+	      dialog_box.dialog("option", "title", heading.text());
+        $("body").css("cursor", "auto");
+      });
+
+			// set the appropriate height/width/position of dialog.
+      dialog_box.dialog("option", "height", "auto");
+			dialog_box.dialog("option", "width", Math.max(($(window).width() /2), 45));
+	    dialog_box.dialog("option", "position", ['center', 75]);
+      dialog_box.dialog("open").dialog("moveToTop");
+
+      return false; // do not execute default href visit
+    });
+    
+  });
+
 });
 
 function update_item_list_numbers_and_classes(){
