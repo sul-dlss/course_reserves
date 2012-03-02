0d7ac712e5e689cca96d73b8f7fcd1ccc82fdffc
Jonathan Lavigne <jlavigne@stanford.edu>
2012-03-01 18:10:09 -0800

    Adding symphony import code, fixture, and tests

---
+++ lib/symphony_import.rb
@@ -0,0 +1,159 @@
+module SymphonyImport
+  
+  # Take a file name, read the lines into an array and remove newlines, then
+  # then call get_course_info to get array of courses and course list that we
+  # use to create database records
+  def process_import_file(file_path)
+    
+    entries = File.readlines(file_path)
+    entries.map {|x| x.chomp! }    
+    
+    courses, course_lists = get_course_info( entries )
+    
+    # Add the item list to the course hash    
+    courses.each do |key, value|
+      courses[key] = value.merge!({:item_list => course_lists[key]}) 
+    end   
+    
+    # Iterate over the courses hash to create records
+    courses.each do |key, value|
+      r = Reserve.create(value)
+      r.save!
+    end
+    
+    # Just for now for testing
+    return courses
+    
+  end # process_import_file
+  
+  
+  # Take a line and return cid - instructor ID as key plus hash of all pipe-delimited fields
+  def get_entry_hash_and_key( line )
+
+    fields = line.split(/\|/)
+    # Keys are from https://consul.stanford.edu/display/NGDE/Symphony+Course+Reserves+Data+spec but will change somewhat
+    # Added :instructor_sunet_id and :item_title at end for nowpr
+    keys = [:reserve_desk, :resctl_expire_date, :resctl_status, :ckey, :barcode, :home_location, 
+            :current_location, :item_reserve_status, :title, :loan_period, :reserve_expire_date, :reserve_stage, 
+            :course_id, :course_name, :term, :instructor_lib_id, :instructor_univ_id, :instructor_name, :instructor_sunet_id,
+            :instructor_email, :instructor_phone
+            ]
+    fields_hash = Hash[*keys.zip(fields).flatten]
+    fields_hash[:instructor_sunet_id].downcase!
+    entry_key = fields_hash[:course_id] + '-' + fields_hash[:instructor_sunet_id]
+
+    return entry_key, fields_hash
+
+  end # get_entry_hash_and_key
+  
+  
+  # Add an entry for a course to courses hash with cid + instructor ID as key
+  def add_to_courses( courses, key, fields_hash )
+
+     courses[key] = { :cid => fields_hash[:course_id],
+                      :sid => '01',
+                      :desc => fields_hash[:course_name],
+                      :library => fields_hash[:reserve_desk],
+                      :term => fields_hash[:term],
+                      :contact_name => fields_hash[:instructor_name],
+                      :contact_phone => fields_hash[:instructor_phone],
+                      :contact_email => fields_hash[:instructor_email],
+                      :instructor_names => fields_hash[:instructor_name],
+                      :instructor_sunet_ids => fields_hash[:instructor_sunet_id]
+                    }
+
+     return courses
+
+  end # add_to_courses
+  
+  # Add an item hash to a course list and return the list
+  def add_to_course_lists( list, fields_hash )
+    
+    # :title
+    # :ckey
+    # :comment
+    # :copies
+    # :personal
+    # :purchase
+    # :loan_period
+    if fields_hash[:home_location] == 'INSTRUCTOR'
+      personal = "true"
+    else
+      personal = ""
+    end
+      
+
+    list <<  { :ckey => fields_hash[:ckey], :title => fields_hash[:title], :loan_period => fields_hash[:loan_period], :personal => personal }
+
+    return list
+
+  end # add_to_course_list
+  
+  
+  # Take an array of pipe-delimited entries exported from Symphony, put data into
+  # appropriate hashes keyed by course_id + instructor_id, return hashes
+  # of courses and course lists
+  def get_course_info( entries )
+
+    courses = {}
+    course_lists = {}
+    copy_count = {}
+
+    # Iterate over all lines and add data to courses, course_lists, and copy_count hashes
+    entries.each do |line|
+
+      key, fields_hash = get_entry_hash_and_key(line)
+
+      # Add entry to courses hash if not there already
+      if ! courses.has_key?(key)
+        courses = add_to_courses( courses, key, fields_hash )
+      end # add to course
+
+      # Add new hash to lists within courses_lists hash for key
+      if course_lists.has_key?(key)
+        course_lists[key] = add_to_course_lists(course_lists[key], fields_hash)
+      else
+        list = []
+        course_lists[key] = add_to_course_lists( list, fields_hash )
+      end # add to course_list
+
+      # Add to copy count for each ckey for appropriate course
+      if copy_count.has_key?(key) # have entry for course
+         if copy_count[key].has_key?(fields_hash[:ckey]) # have entry for ckey so add 1
+           copy_count[key][fields_hash[:ckey]] = copy_count[key][fields_hash[:ckey]] + 1
+         else # no entry for ckey, so add with value 1
+           copy_count[key][fields_hash[:ckey]] = 1
+         end
+      else
+         ckey = {}
+         ckey[fields_hash[:ckey]] = 1
+         copy_count[key] = ckey
+      end # add to copy count
+
+    end # do each line
+
+    # Now add copy count to item list in course_lists hash
+    course_lists.each do |key, value|
+       #puts "key + value here is: " + key.inspect + ' => ' + value.inspect
+       val_arr_new = []
+       value.each do |val_hash_entry|
+         #puts "val_hash_entry is: " + val_hash_entry.inspect
+         count = copy_count[key][val_hash_entry[:ckey]]
+         val_hash_entry[:copies] = count
+         val_arr_new << val_hash_entry
+       end
+       course_lists[key] = val_arr_new.uniq.sort_by { |hsh| hsh[:item_title] }
+    end
+
+    return courses, course_lists
+
+  end # create_course_hashes
+  
+
+end
+  
+  
+  
+  
+  
+  
---
+++ spec/lib/symphony_import_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+require "symphony_import"
+
+class SI_Class
+end
+
+def test_file_path
+  return "#{Rails.root}/spec/fixtures/symphony_import_test.txt"
+end
+  
+
+
+describe "SymphonyImport" do
+  
+  before(:each) do
+    @si_class = SI_Class.new
+    @si_class.extend(SymphonyImport)
+  end
+  
+  describe "parsing an entry line and getting back a key and hash" do
+    it "should take an entry line and return a key and hash of entry elements" do
+      entry_key, entry_hash = @si_class.get_entry_hash_and_key('GREEN-RESV|20120323|CURRENT|8634|36105011709016  |STACKS|GREEN-RESV|ON_RESERVE|White collar; the American middle classes|2H|20120323|ACTIVE|AMSTUD-160|Perspectives on American Identity|WINTER|2555460971|01157452|Gillam, Richard A|RGILLAM|rgillam@stanford.edu|(650) 723-4965|')
+      entry_key.should=='AMSTUD-160-rgillam'
+      entry_hash[:ckey].should=='8634'
+      #entry_hash[:ckey].should=={:reserve_desk=>"GREEN-RESV", :resctl_expire_date=>"20120323", :resctl_status=>"CURRENT", :ckey=>"8634", :barcode=>"36105011709016  ", :home_location=>"STACKS", :current_location=>"GREEN-RESV", :item_reserve_status=>"ON_RESERVE", :loan_period=>"2H", :reserve_expire_date=>"20120323", :reserve_stage=>"ACTIVE", :course_id=>"AMSTUD-160", :course_name=>"Perspectives on American Identity", :term=>"WINTER", :instructor_lib_id=>"2555460971", :instructor_univ_id=>"01157452", :instructor_name=>"Gillam, Richard A", :instructor_sunet_id=>"rgillam", :item_title=>"White collar; the American middle classes."}
+    end
+  end
+  
+  describe "adding an entry to an empty courses hash" do
+    it "should take the courses hash, a key, and a fields hash and return the courses hash with an entry added" do
+      courses = {}
+      key = 'AMSTUD-160-rgillam'
+      fields_hash ={:reserve_desk=>"GREEN-RESV", :resctl_expire_date=>"20120323", :loan_period=>"2H", :course_id=>"AMSTUD-160", :course_name=>"Perspectives on American Identity", :term=>"WINTER", :instructor_lib_id=>"2555460971", :instructor_univ_id=>"01157452", :instructor_name=>"Gillam, Richard A", :instructor_sunet_id=>"rgillam", :item_title=>"White collar; the American middle classes."} 
+      courses = @si_class.add_to_courses( courses, key, fields_hash )
+      courses.length.should == 1
+      courses.has_key?("AMSTUD-160-rgillam").should be_true
+    end
+  end
+  
+  describe "adding an entry to a courses hash that contains an entry" do
+    it "should take the courses hash, a key, and a fields hash and return the courses hash with an entry added" do
+      courses = {"AMSTUD-160-rgillam"=>{:course_id=>"AMSTUD-160", :course_name=>"Perspectives on American Identity", :instructor_id=>"rgillam"}}
+      key = 'AMSTUD-114N-rgillam'
+      fields_hash ={:reserve_desk=>"GREEN-RESV", :resctl_expire_date=>"20120323", :loan_period=>"2H", :course_id=>"AMSTUD-114N", :course_name=>"The American 1960s: Thought, Protest, and Culture", :term=>"WINTER", :instructor_lib_id=>"2555460971", :instructor_univ_id=>"01157452", :instructor_name=>"Gillam, Richard A", :instructor_sunet_id=>"rgillam", :item_title=>"Why we can't wait"} 
+      courses = @si_class.add_to_courses( courses, key, fields_hash )
+      courses.length.should == 2
+      courses.has_key?("AMSTUD-160-rgillam").should be_true
+      courses.has_key?("AMSTUD-114N-rgillam").should be_true
+    end
+  end
+  
+  
+  describe "adding eleven reserves records" do
+    it "should take a file path and call the process_import_file method to add eleven records to the database" do
+      # Just getting back the courses and course_lists hashes for the moment. This creates records in test db acc to test.log
+      # but how do I see them in the db??
+      courses = @si_class.process_import_file(test_file_path)
+      # Note all the following just tests whether the returned courses array has the stuff from the test input file in the right place
+      courses.length.should == 11
+      courses.has_key?('CLASSHIS-114-amemiya').should be_true
+      #puts courses['CLASSHIS-114-amemiya'].inspect
+      courses['CLASSHIS-114-amemiya'][:item_list].length.should==1
+      courses['CLASSHIS-114-amemiya'][:item_list][0][:ckey].should=='32837'
+      Reserve.find_by_cid("CLASSHIS-114").cid.should=='CLASSHIS-114'
+      #Reserve.find(r[:id]).item_list.should be_blank
+      #Reserve.find_by_sunet("amemiya")
+      #courses.each do |key,value|
+      #  value.each do |k2,v2|
+      #    puts "#{k2} => #{v2}"  
+      #  end
+      #  puts '<br>==============' + "<br>\n"
+      #end
+      #puts courses.inspect
+      #courses.has_key?("AMSTUD-160-rgillam").should be_true
+    end    
+  end
+  
+  
+  
+  
+end
