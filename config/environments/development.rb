436072b1d36a7f77e6e25d5bcc26006a97654ef5
Jessie Keck <jessie.keck@gmail.com>
2012-03-05 16:22:38 -0800

    Not loading static assets in dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ CourseReserves::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  
+  # don't serve static files in dev
+  config.serve_static_assets = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
