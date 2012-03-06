29bec2ddc99e482a242e8b3cc7e424bd1589e1e5
Jessie Keck <jessie.keck@gmail.com>
2012-03-05 21:28:11 -0800

    Removing myself as default user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def current_user
-    Rails.env.production? ? request.env["WEBAUTH_USER"] : "jkeck"
+    Rails.env.production? ? request.env["WEBAUTH_USER"] : "jdoe"
   end
   
   helper_method(:current_user)
