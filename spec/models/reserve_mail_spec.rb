46529d1cab45bee18e796de11f18b2a5d8c25643
Jessie Keck <jessie.keck@gmail.com>
2012-03-09 10:18:55 -0800

    Adding in edit reserve link in email.

--- spec/models/reserve_mail_spec.rb
+++ spec/models/reserve_mail_spec.rb
@@ -26,6 +26,10 @@ describe ReserveMail do
       body.should match(/Circ rule: 4HWF-RES/)
       body.should match(/Copies: 2/)
     end
+    it "should have the full edit URL in the email" do
+      email = ReserveMail.first_request(Reserve.create(@reserve_params.merge(:item_list=>[{"ckey"=>"12345", "title"=>"SW Item", "copies"=>"2", "loan_period"=>"4 hours"}])), "test@example.com")
+      email.body.raw_source.should match(/http:\/\/reserves.stanford.edu\/reserves\/1\/edit/)
+    end
   end
   describe "updated_request" do
     it "should return the correct main info for an updated request" do
@@ -41,6 +45,10 @@ describe ReserveMail do
       body.should match(/Contact Name: John Doe/)
       body.should match(/Contact Email: jdoe@example.com/)
       body.should match(/Contact Phone: 555-555-5555/)
+    end
+    it "should have the full edit URL in the email" do
+      email = ReserveMail.updated_request(Reserve.create(@reserve_params), "test@example.com", "DiffText")
+      email.body.raw_source.should match(/http:\/\/reserves.stanford.edu\/reserves\/1\/edit/)
     end
   end
 end
