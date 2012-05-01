07448b5ac4cf2e7171d8f3baa4cf1cc85547ac1b
Jessie Keck <jessie.keck@gmail.com>
2012-05-01 13:15:16 -0700

    Removing tests from app helper spec for old stub code.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,35 +13,4 @@ describe ApplicationHelper do
       item_in_searchworks?({:comment=>"hello", :copies=>"3", :loan_period => "1 day"}).should be_false
     end
   end
-  
-  describe "terms" do
-    # describe "current_term" do
-    #   it "should do something" do
-    #     current_term.should == ""
-    #   end
-    # end
-    describe "future terms" do
-      it "should return 2 terms" do
-        future_terms.length.should == 2
-      end
-      it "should return the two terms after a particular term" do
-        ft = future_terms("Fall")
-        ft.length.should == 2
-        ft.include?("Winter").should be_true
-        ft.include?("Spring").should be_true
-      end
-      it "should handle future terms that split the academic calendar" do
-        ft = future_terms("Spring")
-        ft.length.should == 2
-        ft.include?("Summer").should be_true
-        ft.include?("Fall").should be_true
-      end
-      it "should be able to handle when we are at the end of the academic calendar" do
-        ft = future_terms("Summer")
-        ft.length.should == 2
-        ft.include?("Fall").should be_true
-        ft.include?("Winter").should be_true
-      end
-    end
-  end
 end
