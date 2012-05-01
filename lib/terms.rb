9d78955be69e34c9ac2ebc38a2b4065099f0e0e5
Jessie Keck <jessie.keck@gmail.com>
2012-05-01 13:15:40 -0700

    Adding module for terms.

---
+++ lib/terms.rb
@@ -0,0 +1,23 @@
+module Terms
+    
+  def current_term
+    current_term_hash[:term]
+  end
+  
+  def current_term_hash(date = Date::today)
+    CourseReserves::Application.config.terms.each_with_index do |term, ix|
+      return term if term[:end_date] >= date and CourseReserves::Application.config.terms[ix-1][:end_date] < date
+    end
+  end
+  
+  def future_terms(term = nil)
+    if term.nil?
+      current_term_index = CourseReserves::Application.config.terms.index(current_term_hash)
+    else
+      current_term = CourseReserves::Application.config.terms.collect{|t| t if t[:term] == term}.compact.first
+      current_term_index = CourseReserves::Application.config.terms.index(current_term)
+    end
+    return [CourseReserves::Application.config.terms[current_term_index+1][:term], CourseReserves::Application.config.terms[current_term_index+2][:term]]
+  end
+  
+end
---
+++ spec/lib/terms_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'terms'
+describe "Terms" do
+  include Terms
+  describe "current_term_hash" do
+    it "should return the appropriate term in the middle of a term" do
+      term = current_term_hash(Date.new(2017, 7, 10))
+      term[:term].should == "Summer 2017"
+    end
+    it "should return the appropriate term on the last day of the quarter" do
+      term = current_term_hash(Date.new(2018, 3, 23))
+      term[:term].should == "Winter 2018"
+    end
+    it "should return the appropriate term on the first day of the quarter" do
+      term = current_term_hash(Date.new(2018, 3, 24))
+      term[:term].should == "Spring 2018"
+    end
+  end
+  describe "future terms" do
+    it "should return 2 terms" do
+      future_terms.length.should == 2
+    end
+    it "should return the two terms after a particular term" do
+      ft = future_terms("Winter 2016")
+      ft.length.should == 2
+      ft.include?("Spring 2016").should be_true
+      ft.include?("Summer 2016").should be_true
+    end
+    it "should handle future terms that split the academic calendar" do
+      ft = future_terms("Spring 2016")
+      ft.length.should == 2
+      ft.include?("Summer 2016").should be_true
+      ft.include?("Fall 2016").should be_true
+    end
+    it "should be able to handle when we are at the end of the academic calendar" do
+      ft = future_terms("Summer 2016")
+      ft.length.should == 2
+      ft.include?("Fall 2016").should be_true
+      ft.include?("Winter 2017").should be_true
+    end
+  end
+end
