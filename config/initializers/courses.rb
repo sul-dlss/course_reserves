7ef6fcec47a8aa331fa208b7ea6dc9950bf580e1
Jessie Keck <jessie.keck@gmail.com>
2012-05-01 22:35:19 -0700

    Sending message when there is an issue w/ the current or future terms on application load.

--- config/initializers/courses.rb
+++ config/initializers/courses.rb
@@ -1,3 +1,8 @@
 require "course_work_courses"
+require "terms"
+include Terms
 CourseReserves::Application.config.courses = CourseWorkCourses.new
 CourseReserves::Application.config.courses.all_courses
+if future_terms.length != 2 or current_term.blank?
+  Report.msg(:to=>"searchworks-ops@lists.stanford.edu", :subject => "Issue with CourseReserves terms.", :message=>"There is an issue with the terms in the Course Reserves form. The current term is #{current_term.inspect} and the future terms are: #{future_terms.inspect}").deliver
+end
