13ee454606b400204325cbd34a7c4bd1b89802e2
Chris Beer <cabeer@stanford.edu>
2014-07-29 17:25:18 -0700

    Add squash exception reporting

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_to, '/home/reserves/course_reserves'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/environment_specific_settings.rb}
+set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/squash_exceptions.rb config/initializers/environment_specific_settings.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log lib/course_work_xml tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
