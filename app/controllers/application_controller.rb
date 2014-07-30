c4a0679114e7c5d4d673639b004f6db66e2323ac
Chris Beer <cabeer@stanford.edu>
2014-07-29 17:25:18 -0700

    Add squash exception reporting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  include Squash::Ruby::ControllerMethods
+  enable_squash_client
+
   def current_user
     Rails.env.production? ? request.env["WEBAUTH_USER"] : "jdoe"
   end
