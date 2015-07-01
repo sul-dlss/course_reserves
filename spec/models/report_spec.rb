7fd3942d2990e08af6b8d485f6d01b01b949f4c4
Chris Beer <cabeer@stanford.edu>
2015-07-01 10:52:40 -0700

    Update rspec to new style syntax

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Report do
   it "should send a standard message" do
     r = Report.msg(:to => "test@example.com", :subject => "test email", :message => "This is the body text")
-    r.to.should == ["test@example.com"]
-    r.subject.should == "test email"
-    r.body.raw_source.should == "This is the body text"
+    expect(r.to).to eq(["test@example.com"])
+    expect(r.subject).to eq("test email")
+    expect(r.body.raw_source).to eq("This is the body text")
   end
   it "should send a blank message when no text is proved (e.g. subject is message)" do
     r = Report.msg(:to => "test@example.com", :subject => "test email")
-    r.to.should == ["test@example.com"]
-    r.subject.should == "test email"
-    r.body.raw_source.should be_blank
+    expect(r.to).to eq(["test@example.com"])
+    expect(r.subject).to eq("test email")
+    expect(r.body.raw_source).to be_blank
   end
 end
   
