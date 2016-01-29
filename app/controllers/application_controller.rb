7751943f3abb3d96e635e68211980997c885ee88
Chris Beer <cabeer@stanford.edu>
2016-01-29 11:19:26 -0800

    Update authn/authz to support environment variables in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,25 @@ class ApplicationController < ActionController::Base
   enable_squash_client
 
   def current_user
-    Rails.env.production? ? request.env["WEBAUTH_USER"] : "jdoe"
+    if env['REMOTE_USER']
+      env['REMOTE_USER']
+    elsif Rails.env.development? && ENV['REMOTE_USER']
+      ENV['REMOTE_USER']
+    end
   end
   helper_method(:current_user)
 
   def superuser?
-    Rails.env.production? ? request.env["WEBAUTH_LDAPPRIVGROUP"] && request.env["WEBAUTH_LDAPPRIVGROUP"].include?("sulair:course-resv-admins") : Rails.env.development?
+    privgroups.include?(Settings.workgroups.superuser)
   end
 
+  def privgroups
+    if env['WEBAUTH_LDAPPRIVGROUP']
+      env['WEBAUTH_LDAPPRIVGROUP'].split('|')
+    elsif Rails.env.development? && ENV['WEBAUTH_LDAPPRIVGROUP']
+      ENV['WEBAUTH_LDAPPRIVGROUP'].split('|')
+    else
+      []
+    end
+  end
 end
