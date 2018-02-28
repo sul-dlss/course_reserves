b2f3d173fab90d6115dd30a8288c51b095221a1f
Chris Beer <cabeer@stanford.edu>
2018-02-27 16:17:43 -0800

    Add modern RSpec niceities

--- spec/lib/course_work_courses_spec.rb
+++ spec/lib/course_work_courses_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 require "course_work_courses"
-describe "CourseWorkCourses" do
-  before(:all) do
-    @courses = CourseWorkCourses.new
-  end
+
+RSpec.describe CourseWorkCourses do
+  subject(:courses) { CourseWorkCourses.new }
 
   describe "loading raw XML" do
     it "should be a Nokogiri XML Document" do
-      @courses.raw_xml.each do |xml|
+      courses.raw_xml.each do |xml|
         expect(xml).to be_a_kind_of(Nokogiri::XML::Document)
       end
     end
@@ -19,82 +18,85 @@ describe "CourseWorkCourses" do
     end
   end
   
-  describe "listing all courses" do
-    before(:each) do
-      @course_list = @courses.all_courses
-    end
+  describe "#all_courses" do
+    subject(:course_list) { courses.all_courses }
+
     it "should have all 7 courses in fixture XML" do
-      expect(@course_list.length).to eq(7)
+      expect(course_list.length).to eq(7)
     end
+
     it "should countain the 3 different course titles from the fixture XML" do
-      course_titles = @course_list.map{|c| c[:title] }.uniq
+      course_titles = course_list.map { |c| c[:title] }.uniq
       expect(course_titles.length).to eq(3)
       expect(course_titles).to eq(["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems", "Art/Hist Cross Listed Course"])
     end
   end
   
-  describe "finders" do
+  describe ".find_by_sunet" do
     describe "by SUNet ID" do
       it "should return the subset of courses for the SUNet ID w/ fewer courses" do
-        courses_456 = @courses.find_by_sunet("456")
+        courses_456 = courses.find_by_sunet("456")
         expect(courses_456.length).to eq(2)
         expect(courses_456.map{|c| c[:title]}).to eq(["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"])
       end
+
       it "should return the subset of courses for the SUNet ID that is an instructor for all courses" do
-        courses_123 = @courses.find_by_sunet("123")
+        courses_123 = courses.find_by_sunet("123")
         expect(courses_123.length).to eq(5)
         expect(courses_123.map{|c| c[:title]}.uniq).to eq(["Residential Racial Segregation and the Education of African-American Youth", "Global Positioning Systems"])
       end
+
       it "should be blank when a course doesn't exist with the specified SUNet" do
-        expect(@courses.find_by_sunet("no-sunet")).to be_blank
+        expect(courses.find_by_sunet("no-sunet")).to be_blank
       end
     end
   
-    describe "by class id" do
+    describe ".find_by_class_id" do
       it "should return the appropriate course when passing a class id" do
-        two_section_course = @courses.find_by_class_id("EDUC-237X")
+        two_section_course = courses.find_by_class_id("EDUC-237X")
         expect(two_section_course.length).to eq(2)
         expect(two_section_course.map{|c| "#{c[:cid]}-#{c[:sid]}"}).to eq(["EDUC-237X-01", "EDUC-237X-02"])
       end
+
       it "should be blank when a course doesn't exist with the specified class id" do
-        expect(@courses.find_by_class_id("DOES-NOT-EXIST")).to be_blank
+        expect(courses.find_by_class_id("DOES-NOT-EXIST")).to be_blank
       end
     end
   
-    describe "by class id and section" do
+    describe ".find_by_class_id_and_section" do
       it "should return the appropriate course when passing a class id and section id" do
-        course = @courses.find_by_class_id_and_section("EDUC-237X", "01")
+        course = courses.find_by_class_id_and_section("EDUC-237X", "01")
         expect(course.length).to eq(1)
         expect(course.first[:title]).to eq("Residential Racial Segregation and the Education of African-American Youth")
         expect(course.first[:cid]).to eq("EDUC-237X")
         expect(course.first[:sid]).to eq("01")
       end
       it "should be blank when the class id doesn't exist" do
-        expect(@courses.find_by_class_id_and_section("DOES-NOT-EXIST", "01")).to be_blank
+        expect(courses.find_by_class_id_and_section("DOES-NOT-EXIST", "01")).to be_blank
       end
       it "should be blank when the class id is valid but not the section" do
-        expect(@courses.find_by_class_id_and_section("EDUC-237X", "03")).to be_blank
+        expect(courses.find_by_class_id_and_section("EDUC-237X", "03")).to be_blank
       end
     end
     
     describe "find by class id and sunet" do
       it "should return the appropriate course when passing a class id and sunet" do
-        course = @courses.find_by_class_id_and_sunet("EDUC-237X", "456")
+        course = courses.find_by_class_id_and_sunet("EDUC-237X", "456")
         expect(course.length).to eq(1)
         expect(course.first[:title]).to eq("Residential Racial Segregation and the Education of African-American Youth")
         expect(course.first[:cid]).to eq("EDUC-237X")
       end
       it "should be blank when the class id doesn't exist" do
-        expect(@courses.find_by_class_id_and_sunet("NOT-A-COURSE", "456")).to be_blank
+        expect(courses.find_by_class_id_and_sunet("NOT-A-COURSE", "456")).to be_blank
       end
       it "should be blank when the provided sunet isn't in the course" do
-        expect(@courses.find_by_class_id_and_sunet("EDUC-237X", "654")).to be_blank
+        expect(courses.find_by_class_id_and_sunet("EDUC-237X", "654")).to be_blank
       end
     end
     
     describe "by class id, section, and SUNet ID" do
       it "should return the appripriate course when passing all valid information" do
-        course = @courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "01", "123")
+        course = courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "01", "123")
         expect(course.length).to eq(1)
         expect(course.first[:title]).to eq("Residential Racial Segregation and the Education of African-American Youth")
         expect(course.first[:cid]).to eq("EDUC-237X")
@@ -102,13 +104,13 @@ describe "CourseWorkCourses" do
         expect(course.first[:instructors].map{|i| i[:sunet] }).to include("123")
       end
       it "should return blank when the class id does not exist" do
-        expect(@courses.find_by_class_id_and_section_and_sunet("DOES-NOT-EXIST", "01", "123")).to be_blank
+        expect(courses.find_by_class_id_and_section_and_sunet("DOES-NOT-EXIST", "01", "123")).to be_blank
       end
       it "should return blank when the section does not exist for the given class id" do
-        expect(@courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "03", "123")).to be_blank
+        expect(courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "03", "123")).to be_blank
       end
       it "should return blank when the section SUNet isn't an instructor in for the given course id and section" do
-        expect(@courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "02", "456")).to be_blank
+        expect(courses.find_by_class_id_and_section_and_sunet("EDUC-237X", "02", "456")).to be_blank
       end
     end
     
@@ -119,7 +121,7 @@ describe "CourseWorkCourses" do
       expect(CourseWorkCourses.new("<response><courseclass term='WINTER'><section id='01'></section></courseclass></response>").all_courses).to be_blank
     end
     it "should remove term prefix on class id" do
-      @courses.all_courses.each do |c|
+      courses.all_courses.each do |c|
         expect(c[:cid]).not_to match(/W12/)
       end
     end
@@ -129,20 +131,20 @@ describe "CourseWorkCourses" do
       expect(course[:instructors].first[:name]).to eq("mysunet")
     end
     it "should de-dup courses on the same class id and normalized sunets" do
-      course = @courses.find_by_class_id("AA-272C")
+      course = courses.find_by_class_id("AA-272C")
       expect(course.length).to eq(1)
       expect(course.first[:title]).to eq("Global Positioning Systems")
       expect(course.first[:sid]).to eq("01")
     end
     it "should compute the compound key correctly" do
-      course = @courses.find_by_class_id("ART-102")
+      course = courses.find_by_class_id("ART-102")
       expect(course.length).to eq(1)
       expect(course.first[:cid]).to eq("ART-102")
       expect(course.first[:comp_key]).to eq("ART-102,HIST-201,789")
       expect(course.first[:cross_listings]).to eq("HIST-201")
     end
     it "should computer the cross listed courses properly" do
-      course = @courses.find_by_class_id("HIST-201")
+      course = courses.find_by_class_id("HIST-201")
       expect(course.length).to eq(1)
       expect(course.first[:cid]).to eq("HIST-201")
       expect(course.first[:comp_key]).to eq("ART-102,HIST-201,789")
--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -1,21 +1,21 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Reserve do
-  before(:each) do
-    @reserve_params = {:library => "Green", :immediate=>"true", :contact_name => "John Doe", :contact_phone=>"(555)555-5555", :contact_email=>"jdoe@example.com"}
+RSpec.describe Reserve do
+  let(:reserve_params) do
+    {:library => "Green", :immediate=>"true", :contact_name => "John Doe", :contact_phone=>"(555)555-5555", :contact_email=>"jdoe@example.com"}
   end
   
   describe "editor relationships" do    
     
     it "should generate editor relationships from editor_sunet_ids field for single sunet_id" do
-      reserve = Reserve.create( @reserve_params.merge({ :editor_sunet_ids => 'jlavigne', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :editor_sunet_ids => 'jlavigne', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve.editors.length).to eq(1)
       expect(reserve.editors.first[:sunetid]).to eq('jlavigne')
     end
     
     it "should generate editor relationships from editor_sunet_ids field for multiple sunet_id" do
-      reserve = Reserve.create( @reserve_params.merge({ :editor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :editor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve.editors.length).to eq(2)
       editors = reserve.editors.map{|e| e[:sunetid] }
@@ -24,14 +24,14 @@ describe Reserve do
     end
     
     it "should generate editor relationships from instructor_sunet_ids field for single sunet_id" do
-      reserve = Reserve.create( @reserve_params.merge({ :instructor_sunet_ids => 'asmith', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :instructor_sunet_ids => 'asmith', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve.editors.length).to eq(1)
       expect(reserve.editors.first[:sunetid]).to eq('asmith')   
     end
     
     it "should generate editor relationships from instructor_sunet_ids field for multiple sunet_ids" do
-      reserve = Reserve.create( @reserve_params.merge({ :instructor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :instructor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve.editors.length).to eq(2)
       editors = reserve.editors.map{|e| e[:sunetid] }
@@ -40,16 +40,16 @@ describe Reserve do
     end
     
     it "should udpated editors when we save an item too." do
-      res = Reserve.create(@reserve_params.merge({:instructor_sunet_ids=>'jkeck'}))
+      res = Reserve.create(reserve_params.merge({:instructor_sunet_ids=>'jkeck'}))
       res.save!
       expect(Reserve.find(res[:id]).editors.length).to eq(1)
-      upd_res = Reserve.update(res[:id], @reserve_params.merge({:instructor_sunet_ids=>'jkeck, jlavigne'}))
+      upd_res = Reserve.update(res[:id], reserve_params.merge({:instructor_sunet_ids=>'jkeck, jlavigne'}))
       upd_res.save!
       expect(Reserve.find(res[:id]).editors.length).to eq(2)
     end
     
     it "should generate editor relationships from instructor_sunet_ids & editor_sunet_ids fields for multiple sunet_ids" do
-      reserve = Reserve.create( @reserve_params.merge({ :editor_sunet_ids => 'asmith, bjones', :instructor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :editor_sunet_ids => 'asmith, bjones', :instructor_sunet_ids => 'jlavigne, jkeck', :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve.editors.length).to eq(4)
       editors = reserve.editors.map{|e| e[:sunetid] }
@@ -57,11 +57,11 @@ describe Reserve do
     end
     
     it "should remove editor relationship when we remove a SUNet ID from the list" do
-      res = Reserve.create(@reserve_params.merge({:instructor_sunet_ids=>'jkeck, jlavigne'}))
+      res = Reserve.create(reserve_params.merge({:instructor_sunet_ids=>'jkeck, jlavigne'}))
       res.save!
       expect(res.editors.length).to eq(2)
       expect(Editor.find_by_sunetid("jlavigne").reserves.length).to eq(1)
-      upd_res = Reserve.update(res[:id], @reserve_params.merge({:instructor_sunet_ids=>'jkeck'}))
+      upd_res = Reserve.update(res[:id], reserve_params.merge({:instructor_sunet_ids=>'jkeck'}))
       upd_res.save!
       new_res = Reserve.find(res[:id])
       expect(new_res.editors.length).to eq(1)
@@ -72,13 +72,13 @@ describe Reserve do
 
   describe "item_list serialization" do 
     it "should serialize the item list" do 
-      reserve = Reserve.create( @reserve_params.merge({ :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
+      reserve = Reserve.create( reserve_params.merge({ :cid => 'test_cid', :item_list => [{ :title => 'My Title' }] }) )
       reserve.save!
       expect(reserve[:item_list].first[:title]).to eq('My Title')
     end 
     
     it "should throw an error for TypeMismatch when we serialize the item list with a hash" do 
-      expect {Reserve.create( @reserve_params.merge({ :cid => 'test_cid', :item_list => { :title => 'My Title' } }) )}.to raise_error(ActiveRecord::SerializationTypeMismatch)
+      expect {Reserve.create( reserve_params.merge({ :cid => 'test_cid', :item_list => { :title => 'My Title' } }) )}.to raise_error(ActiveRecord::SerializationTypeMismatch)
     end 
 
   end
