29f10bb73c7cf5683500c1b98994e3d831ecdacb
Chris Beer <cabeer@stanford.edu>
2022-07-18 19:07:57 -0700

    Permit additional YAML classes
    Apparently these are used in some of the historical data

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,18 @@ module CourseReserves
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # List of classes deemed safe to load by YAML.
+    # Rails 7.0.3.1 YAML safe-loading method does not allow all classes
+    # to be deserialized by default:
+    # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
+    config.active_record.yaml_column_permitted_classes = [
+      ActiveSupport::HashWithIndifferentAccess,
+      ActiveSupport::TimeWithZone,
+      ActiveSupport::TimeZone,
+      Date,
+      Symbol,
+      Time
+    ]
   end
 end
