a80b98aff6bda7c7a3bd1d3db45f5f3f85fd389a
Huda Khan <hudakhan@stanford.edu>
2023-05-11 09:52:28 -0400

    changing comment

--- app/services/course_output_writer.rb
+++ app/services/course_output_writer.rb
@@ -6,7 +6,7 @@ class CourseOutputWriter
     @courses = courses
   end
 
-  # Providing parameter for connection enables testing with Faraday connection stub
+  # Generate the file name to use based on term input
   def write
     file_name = "course_#{@file_term}.json"
     # Write the JSON to the course_work_content folder where the app will pick it up later
