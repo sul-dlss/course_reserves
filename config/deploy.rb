216e8c096d40a68cff9e2937ef29c5b51431a151
hudajkhan <hudakhan@stanford.edu>
2023-05-11 14:06:34 -0400

    adding cert and key file to linked files to be picked up from shared config (#476)

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :honeybadger_env, fetch(:stage)
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml config/honeybadger.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/honeybadger.yml config/sul-harvester.cert config/sul-harvester.key}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log config/settings lib/course_work_xml tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
