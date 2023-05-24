bfe115f06f86783869b4bcbd3d56db08396906d0
Huda Khan <hudakhan@stanford.edu>
2023-05-23 20:32:47 -0400

    removing registry harvester course path, removing course work object memoization

--- lib/course_work_courses.rb
+++ lib/course_work_courses.rb
@@ -35,33 +35,11 @@ class CourseWorkCourses
   end
 
   def self.instance
-    @instance ||= CourseWorkCourses.new
+    # @instance ||= CourseWorkCourses.new
+    @instance = CourseWorkCourses.new
   end
 
-  def initialize(file = nil)
-    if Settings.use_course_json
-      initialize_json(file)
-    else
-      initialize_xml(file)
-    end
-  end
-
-  # These two initialize methods are broken out to enable both approaches
-  # until we switch over completely to JSON
-  def initialize_xml(xml)
-    if xml
-      @raw_xml = [Nokogiri::XML(xml)]
-    else
-      @raw_xml = load_xml_from_coursework
-    end
-  end
-
-  def raw_xml
-    @raw_xml ||= self.raw_xml
-  end
-
-  # Handling JSON
-  def initialize_json(json_file)
+  def initialize(json_file = nil)
     if json_file
       @json_files = [JSON.parse(json_file)]
     else
@@ -110,14 +88,8 @@ class CourseWorkCourses
   # TODO: We have tests that are highly sensitive to the order of the courses; this unfortunate
   # logic preserves the bottom-most course from the xml. it's unclear whether this is incidental
   # or a feature.
-  # Breaking out into two approaches.  JSON is picked only if the Setting is present
   def all_courses
-    # if Settings.use_course_json == true
-    if Settings.use_course_json
-      @all_courses ||= process_all_courses(self.json_files).to_a.reverse.uniq(&:key).reverse.to_a
-    else
-      @all_courses ||= process_all_courses_xml(self.raw_xml).to_a.reverse.uniq(&:key).reverse.to_a
-    end
+    @all_courses ||= process_all_courses(self.json_files).to_a.reverse.uniq(&:key).reverse.to_a
   end
 
   # Efficient lookup of course data by compound key (which is used in a few places around the app)
@@ -132,59 +104,6 @@ class CourseWorkCourses
   end
 
   private
-
-  def load_xml_from_coursework
-    if Rails.env.test?
-      return [Nokogiri::XML(File.open("#{Rails.root}/spec/fixtures/course_work.xml", 'r'))]
-    else
-      current = Terms.process_term_for_cw(Terms.current_term)
-      next_term = Terms.process_term_for_cw(Terms.future_terms.first)
-      xml = []
-      ["#{Rails.root}/lib/course_work_xml/courseXML_#{current}.xml", "#{Rails.root}/lib/course_work_xml/courseXML_#{next_term}.xml"].each do |url|
-        xml << Nokogiri::XML(File.open(url, 'r')) if File.exist?(url)
-      end
-      return xml
-    end
-  end
-
-  def process_all_courses_xml(xml_files)
-    return to_enum(:process_all_courses_xml, xml_files) unless block_given?
-
-    xml_files.each do |xml|
-      xml.xpath("//courseclass").each_with_index do |course, idx_course|
-        course_title = course[:title]
-        term = course[:term]
-        cids = []
-        course.xpath("./class").each do |cl|
-          cids << cl[:id].gsub(/^\w{1,2}\d{2}-/, "")
-        end
-        course.xpath("./class").each_with_index do |cl, idx_cl|
-          class_id = cl[:id].gsub(/^\w{1,2}\d{2}-/, "")
-          cl.xpath("./section").each_with_index do |sec, idx_sec|
-            section_id = sec[:id]
-            instructors = []
-            sec.xpath("./instructors/instructor").each do |inst|
-              sunet = inst[:sunetid]
-              name = inst.text
-              name = sunet if inst.text.blank?
-              instructors << { sunet: sunet, name: name }
-            end
-
-            if instructors.present?
-              yield Course.new(
-                title: course_title,
-                term: term,
-                cid: class_id,
-                cids: cids,
-                sid: section_id,
-                instructors: instructors
-              )
-            end
-          end
-        end
-      end
-    end
-  end
 
   # Adding JSON processing
   # Will replace load_xml_from_coursework.  Now loads JSON files generated from MAIS course term and course API requests
